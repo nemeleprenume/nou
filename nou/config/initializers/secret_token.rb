f25b9cfae407088aad9e1723247bb90622b123ab
numele prenume <numeleprenume@yahoo.com>
2011-08-18 23:41:54 +0300

    De Inceput

---
+++ nou/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nou::Application.config.secret_token = '3bd10ec6ed899a1c0ebacb7f38e9f2d792aca8c39551e316edc12a1efe3f42f16d3dd4d2bf0f40ecfb4ae5143f91ad137d3ade960ef90f66b45bb48238084088'
